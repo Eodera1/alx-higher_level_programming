79179359bf32db02e73e9ad6a799e45981c7497d
Eodera1 <ericowino8@gmail.com>
2023-06-07 12:51:20 -0700

    Updated print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 3-print_alphabt.py
-while letter in range(98, 107):
+for letter in range(98, 107):
      if chr(letter) is not 'k' and chr(letter) is not 't':
-         print("{}".format(chr(letter)), end="")
+         print("{:c}".format(chr), end=")
