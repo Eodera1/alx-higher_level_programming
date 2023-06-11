b7143af9f9eef214348bd5f2f90afab55688d2fd
Eodera1 <ericowino8@gmail.com>
2023-06-11 04:45:09 -0700

    Updated python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-     ops = sys.argv[2]
-     if ops != '+' and ops != '-' and ops != '*' and ops != '/':
-         print("Unknown operator. Available operators: /, *, + and and -")
-         sys.exit(1)
+    ops = sys.argv[2]
+    if ops != '+' and ops != '-' and ops != '*' and ops != '/':
+        print("Unknown operator. Available operators: /, *, + and and -")
+        sys.exit(1)
 
     from calculator_1 import div, mul, add, sub
 
