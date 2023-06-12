ea322b91629f549133c6e8173a978184dce3cec2
Eodera1 <ericowino8@gmail.com>
2023-06-12 12:10:52 -0700

    Divisible python

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+multiples = []
+for j in range(len(my_list)):
+    if my_list[j] % 2 == 0:
+        multipls.append(True)
+    else:
+        multiples.append(False)
+
+return (multiples)
